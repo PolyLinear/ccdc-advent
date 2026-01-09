03368d7a347367ecf41577d917c53d16681d2bbc
PolyLinear <bciesla@umass.edu>
2026-01-09 15:46:46 -0500

    multiple breaking changes

--- profile_audit.py
+++ profile_audit.py
@@ -1,56 +1,27 @@
 #!/usr/bin/env python3
 
 import subprocess
-import os
 import json
+import ccdc_script
 import sys
-
-import utils
 
 
 class User:
     def __init__(self, passwd_entry: str):
-        self._name: str = passwd_entry[:passwd_entry.find(':')]
-        self._shell: str = passwd_entry[passwd_entry.rfind(':')+1:].rstrip()
+        self.name = passwd_entry[:passwd_entry.find(':')]
+        self.shell = passwd_entry[passwd_entry.rfind(':')+1:].rstrip()
 
         sudo_proc = subprocess.run(
             ['sudo', '-l', '-U', self.name], capture_output=True)
-        self._privilege: bool = b"not allowed" not in sudo_proc.stdout
-        if self._privilege:
-            utils.err_print(sudo_proc.stdout.decode())
-
-        # checking common login shells
-        self._interactive: bool = False
-        for shell in ["/bash", "/sh", "/zsh", "/ksh", "/csh", "/dash"]:
+        self.privilege = b"not allowed" not in sudo_proc.stdout
+        self.interactive = False
+        for shell in ("/bash", "/sh", "/zsh", "/ksh", "/csh", "/dash"):
             if self.shell.rfind(shell) != -1:
-                self._interactive = True
+                self.interactive = True
                 break
 
-    @property
-    def name(self):
-        """str rep of username """
-        return self._name
 
-    @property
-    def shell(self):
-        """str rep of login shell"""
-        return self._shell
-
-    @property
-    def interactive(self):
-        """bool rep of interactive state; True if interactive, False otherwise"""
-        return self._interactive
-
-    @property
-    def privilege(self):
-        """bool rep of privilege state; True if they are, False otherwise"""
-        return self._privilege
-
-    def json_dict(self):
-        return {"name": self.name, "shell": self.shell, "privilege": self.privilege, "interactive": self.interactive}
-
-
-def get_users(): 
+def get_users():
     result = []
     with open("/etc/passwd", "r") as file:
         for line in file.readlines():
@@ -58,42 +29,29 @@ def get_users():
     return result
 
 
-def output_json(users , file):
+class ProfileAudit(ccdc_script.CCDCScript):
 
-    format = {
-        "users": [],
-        "privilege": [],
-        "interactive": [],
-    }
+    def __init__(self, err_fd=None) -> None:
+        super().__init__(err_fd)
 
-    for user in users:
-        user_dict = user.json_dict()
-        format["users"].append(user_dict)
-        if user.privilege:
-            format["privilege"].append(user_dict)
-        if user.interactive:
-            format["interactive"].append(user_dict)
+    def create_format(self) -> dict:
+        return { "users": { "all": [], "privilege": [], "interactive": [] } }
+        
+    def produce_json(self) -> dict:
+        if not self.check_priv():
+            return {}
 
-    json.dump(format, file, indent=2)
-
-
-def run(stdout=True) -> bool:
-
-    # need effective UID to be 0 (priv)
-    if os.geteuid() != 0:
-        utils.err_print("Please run with root priv", file=sys.stderr)
-        return False
-
-    users = get_users()
-
-    if not stdout:
-        with open(os.uname().nodename+"-profile-audit.json", "w") as f:
-            output_json(users, f)
-    else:
-        output_json(users, sys.stdout)
-    return True
-
+        self.print_err("Starting to get user info")
+        users = get_users()
+        format = self.create_format()
+        for user in users:
+            user_dict = user.__dict__
+            format["users"]["all"].append(user_dict)
+            if user.privilege:
+                format["users"]["privilege"].append(user_dict)
+            if user.interactive:
+                format["users"]["interactive"].append(user_dict)
+        return format
 
 if __name__ == "__main__":
-    if not run(stdout=False):
-        sys.exit(1)
+    json.dump(ProfileAudit().produce_json(), sys.stdout, indent=2)
